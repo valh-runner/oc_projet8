ffbd7a7b93d7bf3437523dff45544670fb9ffe14
valh-runner <valh.trust@free.fr>
2023-12-08 17:00:01 +0100

    manual migration following

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
